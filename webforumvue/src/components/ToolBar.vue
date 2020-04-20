fb06307547f120e61850abec2acdba364b6a46b1
Juan Diego Carvajal Castaño <juandiego.carvajal@hotmail.com>
2020-04-19 19:23:50 -0500

    now in the right side of the navbar only the email of the current user is shown, when clicked, displays the options

--- webforumvue/src/components/ToolBar.vue
+++ webforumvue/src/components/ToolBar.vue
@@ -1,24 +1,30 @@
 <template>
-    <v-toolbar dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
-        <v-toolbar-items>
-        <v-icon style="margin-right:5%">mdi-forum</v-icon>
-        <v-btn to='/users' text>Users</v-btn>
-        <v-btn to='/forums' text>Forums</v-btn>
-        </v-toolbar-items>
+  <v-toolbar dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+    <v-toolbar-items>
+      <v-icon style="margin-right:5%">mdi-forum</v-icon>
+      <v-btn to="/users" text>Users</v-btn>
+      <v-btn to="/forums" text>Forums</v-btn>
+    </v-toolbar-items>
 
-        <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
 
-        <v-toolbar-items>
-        <template v-if="user.loggedIn">
-            <div>{{user.data.name}}</div>
-            <v-btn @click="signOut" text>Sign Out</v-btn>
-        </template>
-        <template v-else>
-            <v-btn to='/logIn' text><v-icon left>mdi-account-circle</v-icon>Login</v-btn>
-            <v-btn to='/signUp' text><v-icon left>mdi-account-plus</v-icon>Sign Up</v-btn>
-        </template>
-        </v-toolbar-items>
-    </v-toolbar>
+    <v-toolbar-items>
+      <template v-if="user.loggedIn">
+        <v-btn text @click="enableOptions = !enableOptions">{{user.data.email}}</v-btn>
+        <v-list v-if="enableOptions">
+                <v-list-item @click="signOut">Sign Out</v-list-item>
+        </v-list>
+      </template>
+      <template v-else>
+        <v-btn to="/logIn" text>
+          <v-icon left>mdi-account-circle</v-icon>Login
+        </v-btn>
+        <v-btn to="/signUp" text>
+          <v-icon left>mdi-account-plus</v-icon>Sign Up
+        </v-btn>
+      </template>
+    </v-toolbar-items>
+  </v-toolbar>
 </template>
 
 <script>
@@ -26,33 +32,47 @@ import { mapGetters } from "vuex";
 import firebase from "firebase";
 
 export default {
-  name: 'App',
+  name: "App",
   computed: {
-    ...mapGetters(
-    {
+    ...mapGetters({
       user: "user"
     })
   },
+  data(){
+    return{
+      enableOptions: false
+    }
+  },
   methods: {
-    signOut() 
-    {
-      firebase.auth().signOut().then(() => 
-      {
-        if(this.$route.name !== 'forums')
-        {
-          this.$router.replace(
-          {
-            name: "forums"
-          })
-        }
-      });
+    signOut() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          if (this.$route.name !== "forums") {
+            this.$router.replace({
+              name: "forums"
+            });
+          }
+        });
     }
   }
-}
+};
 </script>
 
 <style scoped>
 .v-toolbar--prominent .v-toolbar__content {
-    align-items: center
+  align-items: center;
+}
+.v-list-item{
+  background-color: transparent;
+    height: 100%;
+}
+.theme--dark.v-list {
+    /* height: 100%; */
+    background: transparent;
+    color: #fff;
+    padding-bottom: 0;
+    padding-top: 0;
 }
 </style>
